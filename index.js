3b37b4f40a67b5f860a8f5459f6e35a2a3037228
habenhasie <habenhasiet01@gmail.com>
2025-04-09 11:46:00 +0300

    changed port

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const sequelize = require('./config/database'); // Sequelize connection
 const { QueryTypes } = require('sequelize');
 
 const app = express();
-const PORT = process.env.PORT || 8080; // Use port from .env or default to 8080
+const PORT = process.env.PORT || 8070; // Use port from .env or default to 8080
 
 // Middleware
 app.use(express.json());
